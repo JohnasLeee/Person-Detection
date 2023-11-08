3a7a51017ee2cdadd94c36f3c5c25f4619e47bbb
Lee Ying Shen <46188476+JohnasLeee@users.noreply.github.com>
2023-11-08 13:44:30 +0800

    Update run_me.py

--- run_me.py
+++ run_me.py
@@ -1,5 +1,5 @@
 from Detector import *
-
+'''USE EITHER ONE'''
 #modelURL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz'
 #modelURL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz'
 modelURL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet101_v1_1024x1024_coco17_tpu-8.tar.gz'
@@ -14,12 +14,14 @@ imagePath = #PATH OF IMAGE IN YOUR SYSTEM
 videoPath = #PATH OF VIDEO IN YOUR SYSTEM
 
 threshold = 0.5
-person = True
+'''DETECT PERSON ONLY'''
+person = True 
 detector.readClasses(classFile)
 
 detector.downloadModel(modelURL)
 
 detector.loadModel()
 
+''' RUN EITHER ONE '''
 detector.predictImage(imagePath,threshold)
 #detector.predictVideo(videoPath,person,threshold)
